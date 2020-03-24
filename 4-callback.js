7f1d8fc8fcba0a3610e253e8c9330b4e16a92899
dmeadows <meadows.david@siemens.com>
2020-03-24 10:29:55 -0400

    Section 6: The Callback Function

---
+++ 4-callback.js
@@ -0,0 +1,35 @@
+// setTimeout(() => {
+//     console.log(`Two seconds are up`);
+// }, 2000);
+//
+// const names = ['Andrew', 'Tim', 'Yen'];
+// const shortNames = names.filter((name)=> {
+//     return name.length <= 4
+// });
+//
+// const geocode = (address, callback) => {
+//     setTimeout(()=> {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         };
+//         callback(data)
+//     }, 2000)
+// };
+//
+// geocode('Philadelphia', (data) => {
+//     console.log(data);
+// });
+//
+//
+
+const add = (firstNum, secondNum, callback) => {
+    setTimeout(()=> {
+        const sum = firstNum + secondNum;
+        callback(sum);
+    }, 2000);
+};
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
