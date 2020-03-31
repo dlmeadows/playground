765085d6e819af4e656958a0df5ef1b204593f0c
dmeadows <meadows.david@siemens.com>
2020-03-31 11:43:56 -0400

    Section 11: 93. Promise Chaining

--- 8-promises.js
+++ 8-promises.js
@@ -1,20 +1,28 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7,4,11]);
-        reject('Things went wrong, Perfect!');
-    }, 2000)
-})
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+};
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result)
+// add(1, 2).then((sum) => {
+//     console.log(sum);
+//
+//     add(sum, 5).then((sum2) => {
+//         console.log(sum2);
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+// }).catch(() => {
+//     console.log(error);
+// });
+
+add(1, 1).then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+}).then((sum2) => {
+    console.log(sum2);
 }).catch((error) => {
-    console.log('Error!', error)
-})
-
-//
-//                               fulfilled
-//                              /
-// Promise      -- pending -->
-//                              \
-//                                rejected
-//
+    console.log(error);
+});
